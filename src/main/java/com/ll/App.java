1349cf7f6761db7310129a1e5a31ba3d6b69a151
onk108 <wkdtnstjr88@naver.com>
2023-11-07 10:35:56 +0900

    작업

---
+++ src/main/java/com/ll/App.java
@@ -0,0 +1,65 @@
+package com.ll;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class App {
+    Scanner scanner;
+    int lastQuotationId;
+    List<Quotation> quotations;
+
+    App() {
+        scanner = new Scanner(System.in);
+        lastQuotationId = 0;
+        quotations = new ArrayList<>();
+    }
+
+    void run() {
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+
+            String cmd = scanner.nextLine();
+
+            if (cmd.equals("종료")) {
+                break;
+            } else if (cmd.equals("등록")) {
+                actionWrite();
+            } else if (cmd.equals("목록")) {
+                actionList();
+            }
+        }
+    }
+
+    void actionWrite() {
+        System.out.print("명언 : ");
+        String content = scanner.nextLine();
+
+        System.out.print("작가 : ");
+        String authorName = scanner.nextLine();
+
+        lastQuotationId++;
+        int id = lastQuotationId;
+
+        Quotation quotation = new Quotation(id, content, authorName);
+        quotations.add(quotation);
+
+        System.out.printf("%d번 명언이 등록되었습니다.\n", lastQuotationId);
+    }
+
+    void actionList() {
+        System.out.println("번호 / 작가 / 명언");
+
+        System.out.println("----------------------");
+
+        if (quotations.isEmpty())
+            System.out.println("등록된 명언이 없습니다.");
+
+        for (int i = quotations.size() - 1; i >= 0; i--) {
+            Quotation quotation = quotations.get(i);
+            System.out.printf("%d / %s / %s\n", quotation.id, quotation.authorName, quotation.content);
+        }
+    }
+}
---
+++ src/main/java/com/ll/Quotation.java
@@ -0,0 +1,14 @@
+
+package com.ll;
+
+public class Quotation {
+    int id;
+    String content;
+    String authorName;
+
+    public Quotation(int id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
